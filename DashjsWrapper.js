91bbb0495066717ce87eeeb649a2aacab308a3ab
Ruslan Gilmutdinov <ruslandinov@gmail.com>
2016-03-23 15:53:57 +0100

    prefer returning from method over big if body

--- DashjsWrapper.js
+++ DashjsWrapper.js
@@ -16,23 +16,25 @@ class DashjsWrapper {
 
     _onManifestLoaded ({ data }) {
 
-        if (data.url !== this._manifestUrl) {
-
-            this._manifestUrl = data.url;
-
-            //TODO: dispose previous module if it exists
-
-            let mediaMap = new MediaMap(this._manifestHelper);
-            let playerInterface = new PlayerInterface(this._player, this._manifestHelper);
-
-            let p2pConfig = {
-                streamrootKey: "ry-yecv4ugi",
-                debug: true
-            };
-            window.streamrootDownloader = new window.Streamroot.Downloader(playerInterface, this._manifestUrl, mediaMap, p2pConfig, SegmentView, this._videoElement) // TODO: Remove this global definition
-
-            this._player.extend("FragmentLoader", SRFragmentLoader, true);
+        //TODO: we don't know if this event may fire on live streams with same manifest url. if it doesn't, we should remove this check
+        if (data.url === this._manifestUrl) {
+            return;
         }
+
+        this._manifestUrl = data.url;
+
+        //TODO: dispose previous module if it exists
+
+        let mediaMap = new MediaMap(this._manifestHelper);
+        let playerInterface = new PlayerInterface(this._player, this._manifestHelper);
+
+        let p2pConfig = {
+            streamrootKey: "ry-yecv4ugi",
+            debug: true
+        };
+        window.streamrootDownloader = new window.Streamroot.Downloader(playerInterface, this._manifestUrl, mediaMap, p2pConfig, SegmentView, this._videoElement) // TODO: Remove this global definition
+
+        this._player.extend("FragmentLoader", SRFragmentLoader, true);
     }
 }
 
