fc418f2465bf93ff484406f0a82424f4a4a77746
Ruslan Gilmutdinov <ruslandinov@gmail.com>
2016-03-31 18:57:56 +0200

    added Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function(grunt) {
+    require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+
+    grunt.registerTask('default', ['browserify']);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        browserify: {
+            main: {
+                src: 'lib/DashjsWrapper.js',
+                dest: 'dist/dashjs-wrapper.debug.js',
+                options:  {
+                    browserifyOptions: {
+                        standalone: 'DashjsWrapper',
+                        debug: true
+                    },
+                    watch: true,
+                    keepAlive: true
+                }
+            },
+            dist: {
+                src: 'lib/DashjsWrapper.js',
+                dest: 'dist/dashjs-wrapper.js',
+                options:  {
+                    browserifyOptions: {
+                        standalone: 'DashjsWrapper',
+                        debug: false,
+                    },
+                    watch: false,
+                    keepAlive: false,
+                }
+            }
+        },
+        uglify: {
+            options: {
+                mangle: true,
+                compress: {
+                    drop_console: true
+                },
+                beautify: false
+            },
+            dist: {
+                files: {
+                    'dist/dashjs-wrapper.min.js': 'dist/dashjs-wrapper.js'
+                }
+            }
+        }
+    });
+
+
+    grunt.registerTask('build', 'build dist script', ['browserify:dist', 'uglify:dist']);
+
+
+}
